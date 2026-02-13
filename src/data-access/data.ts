f79e5bfffd870bc699e37b3f29497f39924523d8
Michael679089 <purplecheriandres@gmail.com>
2026-02-13 17:29:31 +0800

    v.0.0.18: using json instead for display of my projects

--- src/data-access/data.ts
+++ src/data-access/data.ts
@@ -1,16 +1,5 @@
-import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import projects from '../data/projects.json';
 
 export async function getData() {
-    const DBClient = new DynamoDBClient({
-        region: "ap-southeast-2",
-        // Credentials picked up automatically from AWS_ROLE_ARN + AWS_WEB_IDENTITY_TOKEN_FILE
-        // (GitHub Actions OIDC provides these)
-    });
-
-    const command = new QueryCommand({
-        TableName: "project-list",
-    });
-
-    const response = await DBClient.send(command);
-    return response;
+    return projects;
 };
