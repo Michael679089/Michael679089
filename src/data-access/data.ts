0f5a60f1345041bb5db73ca03176b133f7db006d
Michael679089 <purplecheriandres@gmail.com>
2026-02-13 17:23:18 +0800

    v0.0.17: changed region in my dynamoDBClient

--- src/data-access/data.ts
+++ src/data-access/data.ts
@@ -2,7 +2,7 @@ import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
 
 export async function getData() {
     const DBClient = new DynamoDBClient({
-        region: "us-east-1",
+        region: "ap-southeast-2",
         // Credentials picked up automatically from AWS_ROLE_ARN + AWS_WEB_IDENTITY_TOKEN_FILE
         // (GitHub Actions OIDC provides these)
     });
